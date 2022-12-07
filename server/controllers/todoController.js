698039246a81256db928860023f24bcb042cae51
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-12-07 08:42:20 -0600

    created route to create or save a todo

---
+++ server/controllers/todoController.js
@@ -0,0 +1,16 @@
+const { createNewTodo } = require("../services/todoService");
+const { responseHandler } = require("../utils/responseHandler");
+
+const createTodo = async (req, res) => {
+  let saveTodo = await createNewTodo(req.body);
+
+  return saveTodo[0]
+    ? responseHandler(res, "Todo saved successfully", 201, true, saveTodo[1])
+    : responseHandler(res, "Error saving Todo", 400, false, "");
+};
+
+module.exports = {
+  createTodo,
+  getAllTodos,
+  deleteTodo,
+};
---
+++ server/services/todoService.js
@@ -0,0 +1,17 @@
+const Todo = require("../models/todoModel");
+const { translateError } = require("../utils/mongo_helper");
+
+const createNewTodo = async ({ todo }) => {
+  try {
+    let newTodo = new Todo({ todo });
+    if (await newTodo.save()) {
+      return [true, newTodo];
+    } else {
+      return [false, "todo could not be saved"];
+    }
+  } catch (err) {
+    return [false, translateError(err)];
+  }
+};
+
+module.exports = { createNewTodo };
