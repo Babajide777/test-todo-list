6f9b0cb797fdff9f36523c0c1446755b2b65ad05
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-12-07 06:47:29 -0600

    styling for todo input done

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -4,13 +4,17 @@ import { BsFillPlusSquareFill } from "react-icons/bs";
 const MainComponent = () => {
   return (
     <main className="flex items-center justify-center w-[100vw] h-[100vh] bg-gradient-to-b from-[#68EACC] to-[#497BE8]">
-      <section>
-        <h1>Todo App</h1>
-        <div>
-          <input type="text" placeholder="Enter a new Todo"></input>
+      <section className="bg-white shadow-[0px 10px 15px rgba(0, 0, 0, 0.1)] w-[90%] md:w-[400px] p-6 rounded-xl">
+        <h1 className="font-bold text-4xl mb-4">Todo App</h1>
+        <div className="flex justify-between items-center mb-8">
+          <input
+            type="text"
+            placeholder="Enter a new Todo"
+            className="border-[#ccc] border-[1px] w-[80%] p-2"
+          ></input>
           <BsFillPlusSquareFill
             color="#8E49E8"
-            size="3rem"
+            size="2.5rem"
           ></BsFillPlusSquareFill>
         </div>
         <p>
