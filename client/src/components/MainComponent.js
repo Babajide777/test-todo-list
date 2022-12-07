d0a9d418040a412de2fe73ad6eda40f14f217faa
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-12-07 12:11:45 -0600

    added logic to create todo

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BsFillPlusSquareFill } from "react-icons/bs";
 import SingleTodo from "./SingleTodo";
 
 const MainComponent = () => {
+  const inputEl = useRef(null);
+
+  const handleNewTodo = () => {
+    let todoValue = inputEl.current.value;
+    if (todoValue !== "") {
+      fetch("http://localhost:4000/api/create", {
+        method: "POST",
+        body: JSON.stringify({ todo: todoValue }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        // .then((res) => setfirst(!first))
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <main className="flex items-center justify-center w-[100vw] h-[100vh] bg-gradient-to-b from-[#68EACC] to-[#497BE8]">
       <section className="bg-white shadow-[0px 10px 15px rgba(0, 0, 0, 0.1)] w-[90%] md:w-[400px] p-6 rounded-xl">
@@ -12,12 +30,14 @@ const MainComponent = () => {
             type="text"
             placeholder="Enter a new Todo"
             className="border-[#ccc] border-[1px] w-[85%] rounded-md p-2"
+            ref={inputEl}
           ></input>
           <BsFillPlusSquareFill
             color="#8E49E8"
             size="2.5rem"
             className="cursor-pointer hover:opacity-50"
-          ></BsFillPlusSquareFill>
+            onClick={handleNewTodo}
+          />
         </div>
         <ul>
           <SingleTodo todo="Jide" />
