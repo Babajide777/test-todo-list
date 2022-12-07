46a8c030c22a79540f12e01d1cdf839f2abe9662
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-12-07 12:38:51 -0600

    added logic to delete a specific todo

--- client/src/components/SingleTodo.js
+++ client/src/components/SingleTodo.js
@@ -2,6 +2,18 @@ import React from "react";
 import { MdDeleteOutline } from "react-icons/md";
 
 const SingleTodo = ({ todo, _id, first }) => {
+  const handleClick = () => {
+    fetch(`http://localhost:4000/api/deletetodo/${_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => first())
+      .catch((err) => console.log(err));
+  };
+
   return (
     <li className="flex justify-between items-center bg-[#f2f2f2] py-3 rounded-md mb-6 px-2">
       <p className="text-xl">{todo}</p>
@@ -9,6 +21,7 @@ const SingleTodo = ({ todo, _id, first }) => {
         color="#fff"
         size="1.5rem"
         className="bg-[#8e49e8] cursor-pointer hover:opacity-50"
+        onClick={handleClick}
       />
     </li>
   );
